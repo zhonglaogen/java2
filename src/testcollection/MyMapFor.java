314eecc32973d09f32ccce5c8cac05950b425781
zhonglianxi <ZLXzlxxs98@163.com>
2019-07-09 20:27:38 +0800

    add map

--- src/testcollection/MyMapFor.java
+++ src/testcollection/MyMapFor.java
@@ -3,17 +3,17 @@ package testcollection;
 public class MyMapFor {
     public static void main(String[] args) {
         //map遍历的四种方式
-//        //@1
+//        //@1第一种：普遍使用，二次取值
 //        for(String key:options.keySet()){
 //            System.out.println(key+options.get(key));
 //        }
 //        Iterator<Map.Entry<String, Method>> iterator = options.entrySet().iterator();
-//        //@2
+//        //@2通过Map.entrySet使用iterator遍历key和value
 //        while (iterator.hasNext()){
 //            Map.Entry entry=iterator.next();
 //            System.out.println(entry.getKey()+""+entry.getValue());
 //        }
-//        //@3
+//        //@3推荐，尤其是容量大时
 //        for(Map.Entry<String,Method> entry:options.entrySet()){
 //            System.out.println(entry.getKey()+""+entry.getValue());
 //        }
