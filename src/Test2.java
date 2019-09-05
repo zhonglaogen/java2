49bf53ecc00049fb669ec4512dd9acbb7c171a17
zhonglaogen <ZLXzlxxs98@163.com>
2019-09-05 18:21:41 +0800

    add map

--- src/Test2.java
+++ src/Test2.java
@@ -1,4 +1,10 @@
+import sun.instrument.InstrumentationImpl;
+
+import java.lang.instrument.Instrumentation;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 class Myt{
@@ -18,7 +24,23 @@ class Myt{
 }
 public class Test2 {
     public static void main(String[] args) {
-        new Myt();
+
+
+        Random random = new Random(100);
+        Random random1 = new Random(100);
+        int i = random.nextInt();
+        int ii=random1.nextInt();
+        int iii=random.nextInt();
+
+        int i2 = new Random().nextInt(100);
+        int i3 = new Random().nextInt(100);
+        int i4 = new Random().nextInt(100);
+        System.out.println(i);
+        System.out.println(ii);
+        System.out.println(iii);
+//        System.out.println(i2);
+//        System.out.println(i3);
+//        System.out.println(i4);
 
 
     }
